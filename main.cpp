02231e3ad6540caf177c83450e76e0690313df14
Hama <wujublade@gmail.com>
2023-10-17 14:46:22 -0400

    Create main.cpp
    started the user input gathering code.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout; using std::cin;
+#include <string>
+using std::string;
+#include <stringstream>
+using std::istringstream; using std::ostringstream;
+
+
+
+
+int maint(){
+  string headerStr = "**********C A L C U L A T O R**********\n";
+  string inputQ = "Enter calculation seperated by space: \n"
+  cout << headerStr << inputQ;
+
+  istringstream iss;
+  cin >> iss;
+
+  
+  
+
+return 0;
+}
