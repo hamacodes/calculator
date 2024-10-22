0f4c6d42b99d1ce99841389c693bbfad92de8f26
Hama <pashazad@msu.edu>
2024-10-22 15:14:26 -0400

    Add run() method to Calculator class

---
+++ Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "Calculator.h"
+#include "Operations.h"
+
+
+void Calculator::run() {
+  std::string input;
+  double num1, num2;
+  char op;
+
+}
