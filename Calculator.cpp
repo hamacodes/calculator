ca72ddda73e213c7f12fc5efca7abd445d0c0335
Hama <pashazad@msu.edu>
2024-10-22 15:17:24 -0400

    Add user instructions to Calculator::run() method

--- Calculator.cpp
+++ Calculator.cpp
@@ -11,4 +11,8 @@ void Calculator::run() {
   double num1, num2;
   char op;
 
+  std::cout << "Alas, Hama's CPP Calculator\n";
+  std::cout << "Enter expressions in the format: [number] [operator] number\n";
+  std::cout << "For example: 3 + 4\n";
+  std::cout << "Type 'exit' to quit.\n";
 }
