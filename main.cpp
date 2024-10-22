e55c342ab19e0198bf33fdbf2f15720ee862837b
Hama <pashazad@msu.edu>
2024-10-22 15:34:52 -0400

    Refactor main.cpp to use Calculator class for user input and arithmetic operations

--- main.cpp
+++ main.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
-using std::cout; using std::cin;
 #include <string>
-using std::string;
 #include <stringstream>
-using std::istringstream; using std::ostringstream;
+
+#include "Calculator.h"
 
 
-
-
-int maint(){
-  string headerStr = "**********C A L C U L A T O R**********\n";
-  string inputQ = "Enter calculation seperated by space: \n"
-  cout << headerStr << inputQ;
-
-  istringstream iss;
-  cin >> iss;
-
-  cout << iss; 
-
-  
-  
-
-return 0;
+int main() {
+  Calculator calculator;
+  calculator.run();
+  return 0;
 }
